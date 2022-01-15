6d3b8f45ce18d0bf0cb6bfa705caf3e79f564011
Trans <73827780+TranSMS@users.noreply.github.com>
2022-01-15 17:30:45 +0800

    Update MySet.py

--- M2TS_model/MySet.py
+++ M2TS_model/MySet.py
@@ -5,25 +5,19 @@ from torch.utils.data import Dataset, Sampler
 class MySet(Dataset):
 
     # 读取数据
-    def __init__(self, adj, X, a2, a3, a4, a5, a6, a7, a8, a9, a10, enc_inputs, dec_inputs, dec_outputs):
+    def __init__(self, adj, X, a2, a3, enc_inputs, dec_inputs, dec_outputs):
         self.adj = adj
         self.X = X
         self.a2 = a2
         self.a3 = a3
-        self.a4 = a4
-        self.a5 = a5
-        self.a6 = a6
-        self.a7 = a7
-        self.a8 = a8
-        self.a9 = a9
-        self.a10 = a10
+
         self.enc_inputs = enc_inputs
         self.dec_inputs = dec_inputs
         self.dec_outputs = dec_outputs
     # 根据索引返回数据
 
     def __getitem__(self, idx):
-        return self.adj[idx], self.X[idx], self.a2[idx], self.a3[idx], self.a4[idx], self.a5[idx], self.a6[idx], self.a7[idx], self.a8[idx], self.a9[idx], self.a10[idx], self.enc_inputs[idx], self.dec_inputs[idx], self.dec_outputs[idx]
+        return self.adj[idx], self.X[idx], self.a2[idx], self.a3[idx], self.enc_inputs[idx], self.dec_inputs[idx], self.dec_outputs[idx]
 
     # 返回数据集总长度
     def __len__(self):
