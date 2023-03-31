b81bd228b1d9669b10592e3d938a759cf71abc40
Trans <73827780+TranSMS@users.noreply.github.com>
2023-03-31 10:12:21 +0800

    Update get_A.py

--- Model/get_A.py
+++ Model/get_A.py
@@ -5,7 +5,7 @@ import torch
 import scipy.sparse as sp
 from scipy import sparse
 
-
+# 获取AST相应的邻接矩阵及幂次方矩阵
 def read_batchA(ast_file, max_node):
     file = open(ast_file, 'r', encoding='utf-8')
     papers = []
@@ -27,8 +27,6 @@ def read_batchA(ast_file, max_node):
             for child in id_ch[id]:
                 # fo.write(str(id)+'\t'+str(child)+'\n')
                 edgelist.append((id, child))
-
-
         edgelist = []
         for id in id_ch:
             for child in id_ch[id]:
@@ -58,42 +56,32 @@ def read_batchA(ast_file, max_node):
         a3 = a.dot(a2)
         a4 = a.dot(a3)
         a5 = a.dot(a4)
-
-
         A2 = normalize_data(a2)
         A2 = np.array(A2)
         A2 = sparse.csr_matrix(A2)
         A2 = torch.FloatTensor(np.array(A2.todense()))
         # A2 = sparse_mx_to_torch_sparse_tensor(A2)
         aa2.append(A2)
-
         A3 = normalize_data(a3)
         A3 = np.array(A3)
         A3 = sparse.csr_matrix(A3)
         A3 = torch.FloatTensor(np.array(A3.todense()))
         aa3.append(A3)
-
         A4 = normalize_data(a4)
         A4 = np.array(A4)
         A4 = sparse.csr_matrix(A4)
         A4 = torch.FloatTensor(np.array(A4.todense()))
         aa4.append(A4)
-
         A5 = normalize_data(a5)
         A5 = np.array(A5)
         A5 = sparse.csr_matrix(A5)
         A5 = torch.FloatTensor(np.array(A5.todense()))
         aa5.append(A5)
-
         a = np.array(a, dtype=float)
         adj = normalize(a)
-        # print(adj)
         adj = sp.csr_matrix(adj)
         adj = torch.FloatTensor(np.array(adj.todense()))
-        # print(adj)
         a1.append(adj)
-    # print(len(a1))
-
     return a1, aa2, aa3, aa4, aa5
 
 
@@ -118,6 +106,7 @@ def normalize(mx):
     return mx
 
 
+# 执行归一化过程
 def normalize_data(data):
     list = []
     for line in data:
