27fe72ba2b21eacf57cb1cdfacbb370b425ee6f2
Trans <73827780+TranSMS@users.noreply.github.com>
2023-03-31 11:13:24 +0800

    Update trains.py

--- Model/trains.py
+++ Model/trains.py
@@ -12,11 +12,13 @@ def train(gcn_optimizer, trans_optimizer, data_loader, gcn_model, trans_model, c
         adj, x, a2, a3, a4, a5 = adj.to(device), x.to(device), a2.to(device), a3.to(device), a4.to(device), a5.to(device)
         ast_outputs, ast_embed = gcn_model(x, adj, a2, a3, a4, a5)  
         enc_inputs, dec_inputs, dec_outputs = enc_inputs.to(device), dec_inputs.to(device), dec_outputs.to(device)
-        outputs, enc_self_attns, dec_self_attns, dec_enc_attns, dec_ast_attns = trans_model(enc_inputs, dec_inputs, ast_outputs)  
+        outputs, enc_self_attns, dec_self_attns, dec_enc_attns, dec_ast_attns = trans_model(enc_inputs, dec_inputs, ast_outputs, ast_embed)  
         loss = criterion(outputs, dec_outputs.view(-1))
         trans_optimizer.zero_grad()
+        gcn_optimizer.zero_grad()
         loss.backward()
         trans_optimizer.step()
+        gcn_optimizer.step()
         epoch_loss += loss.item()
     return epoch_loss / len(data_loader)
 
@@ -32,7 +34,7 @@ def evaluate(data_loader, gcn_model, trans_model, criterion, device):
             adj, x, a2, a3, a4, a5 = adj.to(device), x.to(device), a2.to(device), a3.to(device), a4.to(device), a5.to(device)
             ast_outputs, ast_embed = gcn_model(x, adj, a2, a3, a4, a5)
             enc_inputs, dec_inputs, dec_outputs = enc_inputs.to(device), dec_inputs.to(device), dec_outputs.to(device)
-            outputs1, enc_self_attns, dec_self_attns, dec_enc_attns, dec_ast_attns = trans_model(enc_inputs, dec_inputs, ast_outputs)  # 变动
+            outputs1, enc_self_attns, dec_self_attns, dec_enc_attns, dec_ast_attns = trans_model(enc_inputs, dec_inputs, ast_outputs, ast_embed)  # 变动
             loss = criterion(outputs1, dec_outputs.view(-1))
             epoch_loss += loss.item()
     losses = epoch_loss / len(data_loader)
